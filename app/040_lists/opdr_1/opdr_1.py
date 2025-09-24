1fb07e52fa8e14ff178b279402c2c69048aaa614
Jeffinie <samnellestein@gmail.com>
2025-09-24 14:19:52 +0200

    040 opdracht 1

--- app/040_lists/opdr_1/opdr_1.py
+++ app/040_lists/opdr_1/opdr_1.py
@@ -2,8 +2,23 @@
 # Naam student:
 # Groep:
 
-mylist = ...
-dict_1 = ...
-dict_2 = ...
-dict_3 = ...
-dict_4 = ...
+# Vraag de gebruiker om een nummer
+nummer = int(input("Voer een nummer in (0-3): "))
+
+
+
+mylist = []
+dict_1 = {"id": 0, 'voornaam': 'Jan', 'achternaam': 'Jansen'}
+dict_2 = {"id": 1, 'voornaam': 'Piet', 'achternaam': 'Pietersen'}
+dict_3 = {"id": 2, 'voornaam': 'Klaas', 'achternaam': 'Klaassen'}
+dict_4 = {"id": 3, 'voornaam': 'Marie', 'achternaam': 'Jansen'}
+
+mylist.append(dict_1)
+mylist.append(dict_2)       
+mylist.append(dict_3)
+mylist.append(dict_4)
+
+
+# Print de volledige naam van de persoon van de gekozen nummer 
+
+print(mylist[nummer]['voornaam'], mylist[nummer]['achternaam'])
