d9fc5113a10f25182284d02a64a202d5e0de45a4
SamNellestein <97067569@st.deltion.nl>
2025-10-01 11:21:23 +0200

    opdr 4,5,6

--- app/060_loops/opdr_6/opdr_6.py
+++ app/060_loops/opdr_6/opdr_6.py
@@ -6,4 +6,20 @@
 
 # Hier start de for-loop
 
-my_list = []
+pizzas = 'margharita', 'funghi', 'quattro stagioni', 'quattro formaggi', 'tonno', 'calzone'
+print("startlijst:", pizzas)
+
+pizzas.sort()
+print("gesorteerde lijst:", pizzas)
+
+pizzas.append('yo-favorito')
+print("met extra pizza:", pizzas)
+
+pizzas.remove('funghi')
+print("zonder funghi:", pizzas)
+
+print ("eerste pizza:", pizzas[:3])
+
+print ("middelste pizza:", [pizzas[len(pizzas)//2]])
+
+print("laatste 3 pizzas:", pizzas[-3:])
