cd2fb9830c61d74d3f558b42fe70ef440283d169
SamNellestein <97067569@st.deltion.nl>
2025-10-29 09:58:46 +0100

    opdr 1

--- app/080_tekstbestanden/opdr_1/opdr_1.py
+++ app/080_tekstbestanden/opdr_1/opdr_1.py
@@ -3,3 +3,21 @@
 # Groep:
 
 # Jouw code komt hier
+
+vragen = [ 
+    "wat vind je van de huidige regering?", 
+    "wat vind je van de Python-lessen tot nu toe?",
+    "wat vind je de mooiste stad van Nederland?"
+]
+
+antwoorden = []
+for v in vragen: 
+    a = input(v + " ")
+    antwoorden.append(a.strip())
+
+
+with open("enquete_results.txt", "a", encoding="utf-8") as f:
+    for i in range(len(vragen)):
+        f.write(vragen[i] + "\n")
+        f.write(antwoorden[i] + "\n")
+    f.write("\n")  # lege regel tussen enquêtes
