7c2b5b75dd7324c984872652c0cb4308d357b135
SamNellestein <97067569@st.deltion.nl>
2025-10-30 13:37:00 +0100

    opdr2

--- app/080_tekstbestanden/opdr_2/opdr_2.py
+++ app/080_tekstbestanden/opdr_2/opdr_2.py
@@ -3,8 +3,40 @@
 # Groep:
 
 import random
-prompt = "Raad mijn geheime getal \n"
-geheim_getal = random.randint(1, 100)
 
-# De rest moet jij doen.....
+def main():
+    
+    geheim_getal = random.randint(1, 100)
+    pogingen = 0
 
+    print("Raad mijn geheime getal (tussen 1 en 100). Typ 'stop' om te stoppen.")
+    while True:
+        poging = input("Raad mijn geheime getal\n").strip()
+        if poging.lower() == 'stop':
+            print("Je hebt het spel gestopt.")
+            return
+
+        
+        try:
+            getal = int(poging)
+        except ValueError:
+            print("Voer een geldig heel getal in tussen 1 en 100.")
+            continue
+
+        if not (1 <= getal <= 100):
+            print("Gebruik een getal tussen de 1 en 100.")
+            continue
+
+        pogingen += 1
+
+        if getal < geheim_getal:
+            print("hoger")
+        elif getal > geheim_getal:
+            print("lager")
+        else:
+            print(f"Je hebt het getal geraden het is {geheim_getal}!")
+            print(f"Je hebt het in {pogingen} keer gedaan")
+            break
+
+if __name__ == "__main__":
+    main()
